86446d0141159c66ae94d9e0927970fc8e99ba00
CarrBeat <carrbeat@gmail.com>
2022-04-03 17:15:41 +0300

    firstSteps

--- core/src/pong/game/PongGame.java
+++ core/src/pong/game/PongGame.java
@@ -1,31 +1,19 @@
 package pong.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import Screens.FirstScreen;
 
-public class PongGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class PongGame extends Game {
+	public static final int WORLD_WIDTH  = 90;  //160; // 320; // 1280 / PPM;
+	public static final int WORLD_HEIGHT = 160;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new FirstScreen(this));
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
